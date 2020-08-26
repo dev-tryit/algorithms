03459a277499fc709eda1fb5dd5e9ef051e3c31a
imkim1893 <imkim1893@naver.com>
2020-08-26 12:31:18 +0900

    Update 나누어떨어지는숫자배열.java

--- 나누어떨어지는숫자배열.java
+++ 나누어떨어지는숫자배열.java
@@ -6,3 +6,26 @@ class Solution {
         return answer.length != 0 ? answer : new int[]{-1};
     }
 }
+
+// import java.util.*;
+
+// class Solution {
+//     public int[] solution(int[] arr, int divisor) {
+//         PriorityQueue<Integer> pq = new PriorityQueue<>((next,prev)->{
+//             if(prev.intValue()>next.intValue()) return -1;
+//             return 1;
+//         });
+        
+//         for(int v : arr) {
+//             if(v%divisor == 0) pq.add(v);
+//         }
+        
+//         int[] answer = new int[pq.size()];
+//         for(int i=0;i<answer.length;i++) { //여기에 pq.size쓰면 안된다. pq가 작아지므로....... 
+//             answer[i] = pq.poll();
+//         }
+//         return answer.length!=0 ? answer : new int[]{-1};
+//     }
+    
+    
+// }
