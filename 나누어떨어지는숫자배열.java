1411620cf0794c8cc39c5373053df66e4e7c8acc
imkim1893 <imkim1893@naver.com>
2020-08-26 12:32:32 +0900

    Update 나누어떨어지는숫자배열.java

--- 나누어떨어지는숫자배열.java
+++ 나누어떨어지는숫자배열.java
@@ -2,30 +2,21 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] arr, int divisor) {
-        int[] answer = Arrays.stream(arr).filter(i->i%divisor==0).sorted().toArray();
-        return answer.length != 0 ? answer : new int[]{-1};
+        PriorityQueue<Integer> pq = new PriorityQueue<>((next,prev)->{
+            if(prev.intValue()>next.intValue()) return -1;
+            return 1;
+        });
+        
+        for(int v : arr) {
+            if(v%divisor == 0) pq.add(v);
+        }
+        
+        int[] answer = new int[pq.size()];
+        for(int i=0;i<answer.length;i++) { //여기에 pq.size쓰면 안된다. pq가 작아지므로....... 
+            answer[i] = pq.poll();
+        }
+        return answer.length!=0 ? answer : new int[]{-1};
     }
+    
+    
 }
-
-// import java.util.*;
-
-// class Solution {
-//     public int[] solution(int[] arr, int divisor) {
-//         PriorityQueue<Integer> pq = new PriorityQueue<>((next,prev)->{
-//             if(prev.intValue()>next.intValue()) return -1;
-//             return 1;
-//         });
-        
-//         for(int v : arr) {
-//             if(v%divisor == 0) pq.add(v);
-//         }
-        
-//         int[] answer = new int[pq.size()];
-//         for(int i=0;i<answer.length;i++) { //여기에 pq.size쓰면 안된다. pq가 작아지므로....... 
-//             answer[i] = pq.poll();
-//         }
-//         return answer.length!=0 ? answer : new int[]{-1};
-//     }
-    
-    
-// }
