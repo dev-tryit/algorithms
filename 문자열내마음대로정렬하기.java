1ca0dd2ae0502540511a03292dfdbbe574e5535f
imkim1893 <imkim1893@naver.com>
2020-08-28 11:42:41 +0900

    Update 문자열내마음대로정렬하기.java

--- 문자열내마음대로정렬하기.java
+++ 문자열내마음대로정렬하기.java
@@ -2,6 +2,20 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String[] strings, int n) {
+        Arrays.sort(strings, (nextS,prevS)->{            
+            return (prevS.charAt(n)+prevS).compareTo(nextS.charAt(n)+nextS)>0 ? -1 : 1;            
+        });
+        
+        return strings;
+    }
+}
+
+---
+
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] strings, int n) {
         return Arrays.stream(strings).sorted((nextS,prevS)->{
             char prev = prevS.charAt(n);
             char next = nextS.charAt(n);
@@ -14,6 +28,8 @@ class Solution {
         }).toArray(String[]::new);
     }
 }
+
+---
 
 import java.util.*;
 
