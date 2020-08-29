211642fd715734b82e057383ac4e0c570b6df1b7
imkim1893 <imkim1893@naver.com>
2020-08-29 21:37:54 +0900

    Create 문자열내p와y의개수.java

---
+++ 문자열내p와y의개수.java
@@ -0,0 +1,22 @@
+class Solution {
+    boolean solution(String s) {
+        int pCount = StringUtil.countCharOfString(s, 'p');
+        int yCount = StringUtil.countCharOfString(s, 'y');
+        
+        if(pCount==0 && yCount==0) return true;
+        
+        return pCount == yCount;
+    }
+    
+    public static class StringUtil{
+        public static int countCharOfString(String s, char c){
+            int count =0;
+            for(int i=0; i<s.length() ; i++){
+                if((s.charAt(i)+"").equalsIgnoreCase(c+"")) count++;
+            }
+            
+            return count;
+            
+        }
+    }
+}
