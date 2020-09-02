8b5938a93f49425a3732b1cecb81cb9f0e85d34b
imkim1893 <imkim1893@naver.com>
2020-09-02 12:27:39 +0900

    Update 서울에서김서방찾기.java

--- 서울에서김서방찾기.java
+++ 서울에서김서방찾기.java
@@ -4,8 +4,6 @@ import java.util.stream.*;
 class Solution {
     //8배 빠르다.
     public String solution(String[] seoul) {
-        
-        List<String> list = Arrays.asList(seoul);
         for(int i=0; i< seoul.length ; i++) {
             if(seoul[i].equals("Kim")){
                 return String.format("김서방은 %d에 있다", i);
