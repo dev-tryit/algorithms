11b6a9d9cde91afde1da4c8ba7c56a5b62555ebf
imkim1893 <imkim1893@naver.com>
2020-09-02 12:23:33 +0900

    Update 서울에서김서방찾기.java

--- 서울에서김서방찾기.java
+++ 서울에서김서방찾기.java
@@ -2,8 +2,27 @@ import java.util.*;
 import java.util.stream.*;
 
 class Solution {
+    //8배 빠르다.
     public String solution(String[] seoul) {
-        OptionalInt optionalIndex = IntStream.range(0,seoul.length).filter(i->seoul[i].equals("Kim")).findFirst();
-        return String.format("김서방은 %d에 있다", optionalIndex.getAsInt());
+        
+        List<String> list = Arrays.asList(seoul);
+        for(int i=0; i< seoul.length ; i++) {
+            if(seoul[i].equals("Kim")){
+                return String.format("김서방은 %d에 있다", i);
+            }
+        }
+        return "김서방이 없다.";
+    }
+}
+
+---
+
+import java.util.*;
+import java.util.stream.*;
+
+class Solution {
+    public String solution(String[] seoul) {
+        OptionalInt optionalIndex = IntStream.range(0,seoul.length).filter(i->seoul[i].equals("Kim")).findFirst(); //IntStream은 List<OptionalInt>를 반환한다. getAsInt();
+        return String.format("김서방은 %d에 있다", optionalIndex.getAsInt()); //stream은 특정조건에서 break를 하지 못하므로 비효율적이다.
     }
 }
