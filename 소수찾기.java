a6d9cca06702e3cb0831fe429a824d9394e4ce68
imkim1893 <imkim1893@naver.com>
2020-09-03 14:12:10 +0900

    Update 소수찾기.java

--- 소수찾기.java
+++ 소수찾기.java
@@ -7,10 +7,10 @@ class Solution {
         int[] che = IntStream.range(0,n+1).toArray();
         
         for(int i=2 ; i*i<n ; i++) { //1.i*i를 곱해서, n을 넘으면 안해도된다.
-            if(che[i]==0) continue; // 이미 지워진 수라면 건너뛰기
-            for(int j=2*i;j<=n; j+=i) che[j] = 0; // 이미 지워진 숫자가 아니라면, 그 배수부터 출발하여, 가능한 모든 숫자 지우기
+            if(che[i]==-1) continue;
+            for(int j=2*i;j<=n; j+=i) che[j] = -1; // 2. 배수를 지워나간다.
         }
         
-        return (int) Arrays.stream(che).filter(i->i!=0).count()-1; //1제외
+        return (int) Arrays.stream(che).filter(i->i!=-1).count()-2; //3. 0,1을 제외한다.
     }
 }
